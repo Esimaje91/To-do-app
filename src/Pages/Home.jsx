faf10ab1844e25fa12db86dfea57e3e15fd1c94e
Misan Esimaje <esimaje91@gmail.com>
2023-12-27 02:07:45 -0700

    Added footer

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Container from "../Components/Container/Container";
 import Header from "../Components/Header/Header";
 import AddTodo from "../Components/add_todo/AddTodo";
 import Todo_list from "../Components/Todo_list/Todolist";
+import Footer from "../Components/Footer/Footer";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Header />
       <AddTodo />
       <Todo_list />
+      <Footer />
     </Container>
   );
 }
