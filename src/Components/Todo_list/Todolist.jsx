e7bf39f869cb589ed6d52cc67a7e03b3449ced47
Misan Esimaje <esimaje91@gmail.com>
2023-12-27 01:36:10 -0700

    Added task list

--- src/Components/Todo_list/Todolist.jsx
+++ src/Components/Todo_list/Todolist.jsx
@@ -8,6 +8,26 @@ export default function Todolist() {
         <div className="Todolist__item">Buy New Laptop</div>
         <div className="Todolist__delete">Del</div>
       </div>
+
+      <div className="Todolist__row">
+        <div className="Todolist__item">Create a new website</div>
+        <div className="Todolist__delete">Del</div>
+      </div>
+
+      <div className="Todolist__row">
+        <div className="Todolist__item">Buy New Ipad</div>
+        <div className="Todolist__delete">Del</div>
+      </div>
+
+      <div className="Todolist__row">
+        <div className="Todolist__item">Buy New Car</div>
+        <div className="Todolist__delete">Del</div>
+      </div>
+
+      <div className="Todolist__row">
+        <div className="Todolist__item">Read my bible</div>
+        <div className="Todolist__delete">Del</div>
+      </div>
     </div>
   );
 }
