6138456e339393198d93bcd4f8fd2a1287dfde0e
Misan Esimaje <esimaje91@gmail.com>
2023-12-27 00:51:30 -0700

    Adding list todo items

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Container from "../Components/Container/Container";
 import Header from "../Components/Header/Header";
+import AddTodo from "../Components/add_todo/AddTodo";
+import Todo_list from "../Components/Todo_list/Todolist";
 
 export default function Home() {
   return (
     <Container>
       <Header />
+      <AddTodo />
+      <Todo_list />
     </Container>
   );
 }
